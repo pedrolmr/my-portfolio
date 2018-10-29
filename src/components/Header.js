82375fa1e4288bd094938ec8aef13f3a803c198f
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-28 23:57:40 -0400

    building header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <a>Home</a>
+                <a>About</a>
+                <a>Projects</a>
+                <a>Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './Header'
 
 const HomePage = () => {
     return(
-        <div className="home-page"></div>
+        <div className="home-page">
+            <Header />
+        </div>
     );
 }
 export default HomePage;
