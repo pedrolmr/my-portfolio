82bd2d8bf4be1ee924676b21a19abbdfba68d0cb
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-09 16:32:52 -0400

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
-
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import ProjectsPage from './components/ProjectsPage';
-import ContactPage from './components/ContactPage';
-import Footer from './components/Footer' 
-import data from './Data';
 
 class App extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-      data: []
-    }
-  }
-  componentDidMount(){
-    this.setState({ data: data });
-  }
-
   render() {
     return (
       <div className="App">
-       <Header />
-        <Route 
-          exact path="/" 
-          render={props => <HomePage {...props} 
-          data={this.state.data}/>}
-        />
-        <Route path="/about" render={props => <AboutPage {...props} />} />
-        <Route 
-          path="/projects" 
-          render={props => <ProjectsPage {...props} 
-          data={this.state.data}/>} 
-        />
-        <Route path="/contact" render={props => <ContactPage {...props} />} />
-        <Footer />
+        <h1>hello</h1>
       </div>
     );
   }
