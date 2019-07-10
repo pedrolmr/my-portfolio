c6ce1715781dce43e04e7b5fd488f32edd6418a2
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-10 04:21:02 -0400

    styling buttons and form

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,8 +28,8 @@ class Main extends React.Component{
                             <h2>Connect Our Kids</h2>
                             <img src="https://via.placeholder.com/300"/>
                             <View>
-                                <a href="#">View code</a>
-                                <a href="#">View</a>
+                                <LeftViewLink href="#">View code</LeftViewLink>
+                                <RightViewLink href="#">View</RightViewLink>
                             </View>
                         </BoxLeft>
 
@@ -37,29 +37,33 @@ class Main extends React.Component{
                             <h2>S&J Architects</h2>
                             <img src="https://via.placeholder.com/300"/>
                             <View>
-                                <a href="#">View code</a>
-                                <a href="#">View</a>
+                                <LeftViewLink href="#">View code</LeftViewLink>
+                                <RightViewLink href="#">View</RightViewLink>
                             </View>
                         </BoxRight>
                     </BoxSection>
                 </ProjectSection>
 
                 <ContactSection>
-                    <form>
-                        <input type="name" name="name" placeholder="Name" />
-                        <input type="email" name="email" placeholder="Email" />
-                        <textarea type="message" name="message" placeholder="Message" />
-                        <input type="submit" value="Submit" />
-                    </form>
+                    <ContactContainer>
+                        <form action="POST" data-netlify="true">
+                            <Input type="name" name="name" placeholder="Name" />
+                            <Input type="email" name="email" placeholder="Email" />
+                            <div data-netlify-recaptcha="true"></div>
+                            <Textarea type="message" name="message" placeholder="Message" />
+                            <InputButton type="submit" value="Submit" />
+                        </form>
+                    </ContactContainer>
                 </ContactSection>
             </div>
         );
     }
 }
 const TopHeader = styled.div`
-    background: #6441A5;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #2a0845, #6441A5);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #2a0845, #6441A5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: #5433FF;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to top, #A5FECB, #20BDFF, #5433FF);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to top, #A5FECB, #20BDFF, #5433FF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 
     padding:10%;
     background-size: cover;
@@ -67,18 +71,28 @@ const TopHeader = styled.div`
     text-align:center;
     -webkit-clip-path: polygon(100% 0, 100% 90%, 50% 100%, 0 90%, 0 0);
     clip-path: polygon(100% 0, 100% 90%, 50% 100%, 0 90%, 0 0);
+    height:30vh;
 `
 const TopHeaderDiv = styled.div`
     color:#fff;
     width:50%;
-    transform: translate(50%, -20%)
+    transform: translate(50%)
 `
 const AboutSection = styled.div`
     text-align:center;
     color: #fff;
-    background-color: #d4d5d6;
+    background-color:#5433ff;
     padding:40px;
-    margin-top:-40px;
+    margin-top:-45px;
+`
+const Input = styled.input`
+    width:100%
+`
+const Textarea = styled.textarea`
+    width:100%
+`
+const InputButton = styled.input`
+    border: 3px solid blue;
 `
 const ProjectSection = styled.div`
     border: 3px solid black;
@@ -107,12 +121,37 @@ const BoxRight = styled.div`
     height:100%;
 `
 const ContactSection = styled.div`
+    display:flex;
     border: 3px solid blue;
-    height:50vh;
+    justify-content:center;
+
+`
+const ContactContainer = styled.div`
+    border: 3px solid purple;
+    width:50%;
 `
 const View = styled.div`
+    display:flex;
     border: 3px solid blue;
-    height:50vh;
+    flex-direction: row;
+`
+const LeftViewLink = styled.a`
+    display:flex;
+    border: 3px solid black;
+    flex-direction: row;
+    width:100%;
+    justify-content:center;
+    text-decoration:none;
+    color:#000;
+`
+const RightViewLink = styled.a`
+    display:flex;
+    border: 3px solid black;
+    flex-direction: row;
+    width:100%;
+    justify-content:center;
+    text-decoration:none;
+    color:#000;
 `
 
 export default Main;
