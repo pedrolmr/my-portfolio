90ab9eb2222bb7d6fa421fb3a603bc424dcce8bf
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-09 21:27:19 -0400

    created projects section

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,28 @@ class Main extends React.Component{
                     </TopHeaderDiv>
                 </TopHeader>
 
-                <About></About>
+                <AboutSection>
+                    <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus a hendrerit molestie. Morbi laoreet placerat magna, eget pulvinar lectus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus a hendrerit molestie. Morbi laoreet placerat magna, eget pulvinar lectus.</p>
+                </AboutSection>
+
+                <ProjectSection>
+                    <h1>Projects</h1>
+                    <BoxSection>
+                        <BoxLeft>
+                            <h2>Connect Our Kids</h2>
+                            <img src="https://via.placeholder.com/300"/>
+                            <a href="#">View</a>
+                        </BoxLeft>
+
+                        <BoxRight>
+                            <h2>S&J Architects</h2>
+                            <img src="https://via.placeholder.com/300"/>
+                            <a href="#">View</a>
+                        </BoxRight>
+                    </BoxSection>
+                </ProjectSection>
             </div>
         );
     }
@@ -33,13 +54,39 @@ const TopHeader = styled.div`
     clip-path: polygon(100% 0, 100% 90%, 50% 100%, 0 90%, 0 0);
 `
 const TopHeaderDiv = styled.div`
-    color:white;
+    color:#fff;
     width:50%;
     transform: translate(50%, -20%)
 `
-const About = styled.div`
-    border:3px solid pink;
-    padding:20%;
+const AboutSection = styled.div`
+    text-align:center;
+    color: #fff;
+    background-color: #d4d5d6;
+    padding:40px;
+    margin-top:-40px;
 `
+const ProjectSection = styled.div`
+    border: 3px solid black;
+    padding:40px;
+`
+const BoxSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 3px solid purple;
+
+`
+const BoxLeft = styled.div`
+    display:flex;
+    border: 3px solid black;
+    flex-direction:column;
+    align-self:flex-start;
+`
+const BoxRight = styled.div`
+    display:flex;
+    border: 3px solid black;
+    flex-direction:column;
+    align-self:flex-end;
+`
+
 
 export default Main;
