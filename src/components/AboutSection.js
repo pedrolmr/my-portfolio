9ac160d81ab844af1d4c606aedd2d995501d0b4f
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-07-13 22:19:37 -0400

    added icons to the about component using fontawesome

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import bitmoji from '../images/bitmoji.png'
 const AboutSection = () => {
@@ -12,6 +13,11 @@ const AboutSection = () => {
                 </Image>
                 <View>
                     <p>I'm a software developer with a passion for web development. My primary focus is JavaScript but I've worked with multiple programming languages including Python and C. You can take a look at the Projects section to see my work.</p>
+                    <Icons>
+                        <a href="mailto:pedrolmr1998@gmail.com"><FontAwesomeIcon icon='envelope-square' fixedWidth size="2x"/></a>
+                        <a href="https://github.com/pedrolmr" target="_blank"><FontAwesomeIcon icon={['fab', 'github-square']} fixedWidth size="2x"/></a>
+                        <a href="https://www.linkedin.com/in/pedrolmr1998/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} fixedWidth size="2x" /></a>
+                    </Icons>
                 </View>
             </AboutWrapper>
         </About>
@@ -45,7 +51,14 @@ const View = styled.div`
     display:flex;
     flex-direction: column;
     align-self:flex-end;
-    
+`
+const Icons = styled.div`
+    display:flex;
+    justify-content:center;
+
+    a{
+        color:#155799;
+    }
 
 `
 export default AboutSection;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
+library.add(fab, faCheckSquare, faCoffee, faEnvelopeSquare);
+
 ReactDOM.render(<App />, document.getElementById('root'));
