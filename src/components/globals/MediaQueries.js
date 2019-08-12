c092cb0b450f8826d82bfea3426c90aa60c2393e
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-08-12 14:35:57 -0400

    fixed media queries

--- src/components/globals/MediaQueries.js
+++ src/components/globals/MediaQueries.js
@@ -1,31 +1,17 @@
 export const screen = {
     phoneS: (...args) => {
         const styles = args;
-        return `@media (max-width: 320px){${styles}}`
+        // return `@media (max-width: 320px){${styles}}`
+        return `@media only screen and (max-width: 360px){${styles}}`
     },
     phoneM: (...args) => {
         const styles = args;
-        return `@media (max-width: 375px){${styles}}`
+        // return `@media (max-width: 375px){${styles}}`
+        return `@media only screen and (max-width: 375px){${styles}}`
     },
     phoneL: (...args) => {
         const styles = args;
-        return `@media (max-width: 411px){${styles}}`
+        // return `@media (max-width: 411px){${styles}}`
+        return `@media only screen and (max-width: 414px){${styles}}`
     }
 }
-
-/*
-iphone5 320
--deleted GalaxyS5 360
-Pixel2 411
--deleted iphonePlus 414
-iphone6 375
--deleted iphoneX 375
-Ipad 768
-ipad Pro 1024
-*/
-
-/*
-phoneS iphone5 320
-phoneM iphone6 375 
-phoneL pixel2 411
-*/
