75332c216f24009587e8c1f1e9ac3733aa75bddd
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-08-24 23:53:31 -0400

    deleted comments in MediaQueries.js

--- src/components/globals/MediaQueries.js
+++ src/components/globals/MediaQueries.js
@@ -1,17 +1,14 @@
 export const screen = {
     phoneS: (...args) => {
         const styles = args;
-        // return `@media (max-width: 320px){${styles}}`
         return `@media only screen and (max-width: 360px){${styles}}`
     },
     phoneM: (...args) => {
         const styles = args;
-        // return `@media (max-width: 375px){${styles}}`
         return `@media only screen and (max-width: 375px){${styles}}`
     },
     phoneL: (...args) => {
         const styles = args;
-        // return `@media (max-width: 411px){${styles}}`
         return `@media only screen and (max-width: 414px){${styles}}`
     },
     Tablet: (...args) => {
